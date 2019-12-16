1c44370f1758e9d79629e2148dad48824c72a7d8
MRomadhan <muhammad.romadhan060593@gmail.com>
2019-12-16 14:46:12 +0700

    update minors

--- app/controllers/api/v1/students_controller.rb
+++ app/controllers/api/v1/students_controller.rb
@@ -1,15 +1,15 @@
 class Api::V1::StudentsController < ApplicationController
-  before_action :set_student, only: [:show, :update, :destroy]
-  # GET /students
+  before_action :set_student, only: %i[show update destroy]
+
   def index
     @students = Student.all
     render json: @students
   end
-  # GET /students/1
+
   def show
     render json: @student
   end
-  # POST /students
+
   def create
     @student = Student.new(student_params)
     if @student.save
@@ -18,7 +18,7 @@ class Api::V1::StudentsController < ApplicationController
       render json: @student.errors, status: :unprocessable_entity
     end
   end
-  # PATCH/PUT /students/1
+
   def update
     if @student.update(student_params)
       render json: @student
@@ -26,17 +26,17 @@ class Api::V1::StudentsController < ApplicationController
       render json: @student.errors, status: :unprocessable_entity
     end
   end
- # DELETE /students/1
+
   def destroy
     @student.destroy
   end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
+
   def set_student
     @student = Student.find(params[:id])
   end
-  # Only allow a trusted parameter “white list” through.
+
   def student_params
     params.require(:student).permit(:title, :content, :slug)
   end
